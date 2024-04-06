9c7c3f49f490d0bdf9eb1a1d44c307a793e97ef6
anti-duhring <mateus@alternativepayments.io>
2024-04-06 18:41:44 -0300

    Adding processes, process_follow and indexes on migrate.sql

--- migrate.sql
+++ migrate.sql
@@ -9,3 +9,30 @@ CREATE TABLE users (
     updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
     deleted_at TIMESTAMPTZ DEFAULT NULL
 );
+
+CREATE TABLE processes (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    process_number VARCHAR(255) NOT NULL,
+    court VARCHAR(255) NOT NULL,
+    origin VARCHAR(255),
+    judge VARCHAR(255),
+    active_part VARCHAR(255),
+    passive_part VARCHAR(255),
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMPTZ
+);
+
+CREATE TABLE process_follows (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_id UUID NOT NULL,
+    process_id UUID NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMPTZ,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (process_id) REFERENCES processes(id)
+);
+
+CREATE INDEX idx_process_number ON processes (process_number);
+CREATE INDEX idx_user_id ON process_follows (user_id);
+CREATE INDEX idx_process_id ON process_follows (process_id);
